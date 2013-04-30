b05bdb5494a1b824951777d8d54d0a7c1385771f
eightyfour <handroid69@googlemail.com>
2013-04-30 15:52:56 +0300

    Update testReg.js
    First working file regex

--- testReg.js
+++ testReg.js
@@ -4,19 +4,27 @@ var testReg = function(file,key,value,callback){
     var file = file,key = key, value = value;
     var cb = callback;
 //    var reg = new RegExp('([a-zA-Z\d_\.]*)=(.*)[\n\r]*','g');
-    var reg = new RegExp('[\n\r]('+key+'=.*)[\n\r]*','g');
+    var reg = new RegExp('(.*)'+key+'=(.*)([\n\r]*.*)','g');
+
     fs.readFile(file,'utf8', function (err, data) {
         if (err) throw err;
-        var result = key+'='+data.replace(reg, value)+'\n';
+        var result = data.replace(reg,'$1'+key+'='+value+'$3');
         console.log('regex: '+reg);
-        console.log('writeFile: '+result);
-//        fs.writeFile(file,result,'utf8',function(err){
-//            if (err) return console.log(err);
-//            cb('write success');
-//        });
+        console.log(result);
+        console.log('-------\n\n');
+        fs.writeFile(file,result,'utf8',function(err){
+            if (err) return console.log(err);
+            cb('write success');
+        });
     });
 }
 
-testReg(__dirname+'/static/messages.properties','key_2','new string',function(){
+testReg(__dirname+'/static/messages.properties','key_1','THIS IS ONE HERE',function(){
     console.log('success');
+    testReg(__dirname+'/static/messages.properties','key_2','THIS IS TWO HERE',function(){
+        console.log('success');
+        testReg(__dirname+'/static/messages.properties','key_3','THIS IS THREE HERE',function(){
+            console.log('success');
+        });
+    });
 });
