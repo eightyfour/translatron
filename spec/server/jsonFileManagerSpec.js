ce8ebecdebb98ee4a5833fa67f038f1d80443f82
eightyfour <eightyfour@thinkuseful.de>
2015-12-30 18:41:07 +0100

    add json file manager test

---
+++ spec/server/jsonFileManagerSpec.js
@@ -0,0 +1,44 @@
+var fileMgr = require('../../lib/server/legacy/jsonFileManager'),
+    fs = require('fs');
+
+global.projectFolder = __dirname + '/fixtures';
+
+describe('Check that the jsonFileManager works', () => {
+
+    var persistObj = {
+        deepFoo : {
+            foo : "deepFoo foo"
+        },
+        foo : "foo"
+    }, saveResult;
+
+    beforeAll((done) => {
+        fileMgr.saveJSON('/jsonFileManager/test', persistObj, (err) => {
+            saveResult = err;
+        	done();
+        });
+    });
+
+    it("should save a json file", () => {
+        expect(saveResult).toEqual(null);
+    });
+
+    it("should read the JSON from a files", (done) => {
+        fileMgr.getJSON('/jsonFileManager/test.json', (data) => {
+            expect(data).toEqual(persistObj);
+            done();
+        });
+    });
+
+    afterAll((done) => {
+        Promise.resolve()
+            .then(() => new Promise((fulFill, reject) =>
+                fs.unlink(projectFolder + "/jsonFileManager/test.json", fulFill)
+            ))
+            .then(() => new Promise((fulFill, reject) =>
+                fs.rmdir(projectFolder + "/jsonFileManager", fulFill)
+            ))
+            .then(done)
+            .catch((err) => console.log('jsonFileManagerSpec:err', err));
+    });
+});
