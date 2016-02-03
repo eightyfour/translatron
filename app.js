6b0cb49d5d5d63521ad77d0c20e0505d40d8fb47
Torben Fruechtenicht <torben.fruechtenicht@gameduell.de>
2016-02-03 16:14:45 +0100

    got rid of unused conTrade variable, renamed trade variable to websocketServer (because that is what it is)

--- app.js
+++ app.js
@@ -59,119 +59,117 @@ app.use(
 
 var server = app.listen(serverPort);
 
-var conTrade,
-    trade = shoe(function (stream) {
-        "use strict";
+var websocketServer = shoe(function (stream) {
+    "use strict";
 
-        var d = dnode({
-            loadProject : function (projectId, cb) {
-                dao.loadProject(projectId, cb);
-            },
-            saveKey : function(id, projectId, language, keyAndValue, cb) {
-                dao.saveKey(id, projectId, language, keyAndValue, cb);
-            },
-            renameKey : function () {
-                dao.renameKey.apply(null, [].slice.call(arguments));
-            },
-            removeKey : function () {
-                dao.removeKey.apply(null, [].slice.call(arguments));
-            },
-            createNewProject : function (id, path, projectName, obj, cb) {
-                dao.createNewProject(id, path, projectName, obj, cb);
-            },
-            getDirectory : function(dir, cb) {
-                dao.getDirectory(dir, cb);
-            },
-            createNewDirectory : function(id, directoryName, path, cb) {
-                dao.createNewDirectory(id, directoryName, path, cb);
-            },
+    var d = dnode({
+        loadProject : function (projectId, cb) {
+            dao.loadProject(projectId, cb);
+        },
+        saveKey : function(id, projectId, language, keyAndValue, cb) {
+            dao.saveKey(id, projectId, language, keyAndValue, cb);
+        },
+        renameKey : function () {
+            dao.renameKey.apply(null, [].slice.call(arguments));
+        },
+        removeKey : function () {
+            dao.removeKey.apply(null, [].slice.call(arguments));
+        },
+        createNewProject : function (id, path, projectName, obj, cb) {
+            dao.createNewProject(id, path, projectName, obj, cb);
+        },
+        getDirectory : function(dir, cb) {
+            dao.getDirectory(dir, cb);
+        },
+        createNewDirectory : function(id, directoryName, path, cb) {
+            dao.createNewDirectory(id, directoryName, path, cb);
+        },
+        /**
+         * initial call - all client methods are saved here.
+         * returns a id as callback. The client needs this as identifier.
+         */
+        setupClient : function () {
+            // TODO draft: authenticate the client - and pass the name to the setupClient
+            dao.setupClient.apply(null, [].slice.call(arguments));
+        },
+        jsonFileManager : (function () {
+            var ret = {};
+            // lo0ks like a listener :-)
+            Object.keys(jsonFileManager).forEach(function (key) {
+                ret[key] = function () {
+                    jsonFileManager[key].apply(null, [].slice.call(arguments));
+                };
+            });
             /**
-             * initial call - all client methods are saved here.
-             * returns a id as callback. The client needs this as identifier.
+             * currently the merge flag supports only flat merge - TODO deep merge
+             *
+             * @param projectName
+             * @param data
+             * @param merge
+             * @param cb
              */
-            setupClient : function () {
-                // TODO draft: authenticate the client - and pass the name to the setupClient
-                dao.setupClient.apply(null, [].slice.call(arguments));
-            },
-            jsonFileManager : (function () {
-                var ret = {};
-                // lo0ks like a listener :-)
-                Object.keys(jsonFileManager).forEach(function (key) {
-                    ret[key] = function () {
-                        jsonFileManager[key].apply(null, [].slice.call(arguments));
-                    };
-                });
-                /**
-                 * currently the merge flag supports only flat merge - TODO deep merge
-                 *
-                 * @param projectName
-                 * @param data
-                 * @param merge
-                 * @param cb
-                 */
-                ret.saveJSON = function (projectName, data, merge, cb) {
-                    // TODO save this in the description field from the [project name].json
-                    //if (merge) {
-                    //    jsonFileManager.getJSON('/' + projectName + '/project.json', function (oldData) {
-                    //        Object.keys(data).forEach(function (key) {
-                    //            oldData[key] = data[key];
-                    //        });
-                    //        jsonFileManager.saveJSON(projectName + '/project', oldData, cb);
-                    //        console.log('app:saveJSON', projectName, oldData);
-                    //    });
-                    //} else {
-                    //    jsonFileManager.saveJSON(projectName + '/project', oldData, cb);
-                    //    console.log('app:saveJSON', projectName, data);
-                    //}
-                };
+            ret.saveJSON = function (projectName, data, merge, cb) {
+                // TODO save this in the description field from the [project name].json
+                //if (merge) {
+                //    jsonFileManager.getJSON('/' + projectName + '/project.json', function (oldData) {
+                //        Object.keys(data).forEach(function (key) {
+                //            oldData[key] = data[key];
+                //        });
+                //        jsonFileManager.saveJSON(projectName + '/project', oldData, cb);
+                //        console.log('app:saveJSON', projectName, oldData);
+                //    });
+                //} else {
+                //    jsonFileManager.saveJSON(projectName + '/project', oldData, cb);
+                //    console.log('app:saveJSON', projectName, data);
+                //}
+            };
 
-                /**
-                 * Refactor this function - it do multiple jobs
-                 * param: projectName(optional - otherwise take main project.json), callback
-                 */
-                ret.getJSON = function (path, projectName, p1) {
-                    // only a callback is passed
-                    var cb = p1 || projectName;
-                    // if the second parameter a function the client asks for the actual projects in this path
-                    // TODO change it to a separate function call or find a better solution (e.g. server pre render with JADE)
-                    if (typeof projectName === 'function') {
-                        // first bring project JSON up to date
-                        fileManager.readDir(path, function (filesAndFolders) {
-                            var folders = [];
-                            // if the folder is not exists then the value is undefined
-                            if (filesAndFolders.value) {
-                                filesAndFolders.value.forEach(function (folder) {
-                                    if (!folder.d && folder.name !== 'project.json' && /\.json/.test(folder.name)) {
-                                        folders.push(folder.name.split('.')[0]);
-                                    }
-                                });
-                            }
-                            // load the default root project.json
-                            jsonFileManager.getJSON('/project.json', function (data) {
-                                data.projects = folders;
-                                cb(data);
+            /**
+             * Refactor this function - it do multiple jobs
+             * param: projectName(optional - otherwise take main project.json), callback
+             */
+            ret.getJSON = function (path, projectName, p1) {
+                // only a callback is passed
+                var cb = p1 || projectName;
+                // if the second parameter a function the client asks for the actual projects in this path
+                // TODO change it to a separate function call or find a better solution (e.g. server pre render with JADE)
+                if (typeof projectName === 'function') {
+                    // first bring project JSON up to date
+                    fileManager.readDir(path, function (filesAndFolders) {
+                        var folders = [];
+                        // if the folder is not exists then the value is undefined
+                        if (filesAndFolders.value) {
+                            filesAndFolders.value.forEach(function (folder) {
+                                if (!folder.d && folder.name !== 'project.json' && /\.json/.test(folder.name)) {
+                                    folders.push(folder.name.split('.')[0]);
+                                }
                             });
-                        })
+                        }
+                        // load the default root project.json
+                        jsonFileManager.getJSON('/project.json', function (data) {
+                            data.projects = folders;
+                            cb(data);
+                        });
+                    })
 
-                    } else if (/\.prj/.test(projectName)) {
-                        console.log('app:does not support prj files for getJSON calls');
-                    }
-                };
-                return ret;
-            }())
-        });
-
-        d.on('error', function(err) {
-           console.error(err.message, err.stack);
-        });
-
-        d.pipe(stream).pipe(d);
-        conTrade = stream;
-
-        conTrade.on('end', function () {
-            console.log('end');
-        });
+                } else if (/\.prj/.test(projectName)) {
+                    console.log('app:does not support prj files for getJSON calls');
+                }
+            };
+            return ret;
+        }())
     });
-trade.install(server, '/trade');
+
+    // handle errors from processing commands from clients: at least log them
+    // if we did not have this error handler, errors would propagate up the stack and effectively close down the
+    // application
+    d.on('error', function(err) {
+       console.error(err.message, err.stack);
+    });
+
+    d.pipe(stream).pipe(d);
+});
+// "/trade" identifies the websocket connection
+websocketServer.install(server, '/trade');
 
 console.log("start server ", serverPort);
