5cb32fc0028ec69673336ac31700215c0e99ae55
Torben Fruechtenicht <torben.fruechtenicht@gameduell.de>
2016-02-03 12:58:56 +0100

    use a custom name for session cookie. routes cleanup and documentation

--- app.js
+++ app.js
@@ -14,18 +14,26 @@ var projectFolder = __dirname + '/static',
 
 var app = express();
 
-// configure static URL's for static:
-app.use('/dist',express.static(__dirname + '/dist'));
-// and bower files:
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
-// bodyParser must be registered with express app - otherwise no body parsing
-app.use(bodyParser.json());
+// use bodyParser middleware for handling request bodies (express does *not* provide that feature out-of-the-box).
+// since we only have one case of that (POST to /login where username, password are in the body) and that one is url-encoded,
+// we only need that flavor of bodyParser. about the "extended" property, see https://github.com/expressjs/body-parser#bodyparserurlencodedoptions
 app.use(bodyParser.urlencoded({ extended: false }));
+// same for parsing cookie
 app.use(cookieParser());
 
 // activate the LDAP auth
 app.use(require('./lib/server/auth')(app));
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url, req.query, req.user ? req.user.name : '-');
+    next();
+});
+
+// TODO add error handler middleware
+
+// configure routes for static content
+app.use('/dist',express.static(__dirname + '/dist'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 // jade.compileFile is not like a full compilation - it is more like a parsing of the jade code. only the execution
 // of the returned function pointer (with optional passing of locals) will do the actual compilation.
