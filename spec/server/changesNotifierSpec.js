52ca0f3ece4e25e22f14e2fd460ff71f98c93c42
Torben Fruechtenicht <torben.fruechtenicht@gameduell.de>
2016-02-12 12:42:19 +0100

    LODEV-4663 register/deregister are now add/remove + listenerId is not a property of listener

--- spec/server/changesNotifierSpec.js
+++ spec/server/changesNotifierSpec.js
@@ -16,13 +16,13 @@ describe('changesNotifier', () => {
         done();
     });
 
-    describe('registerListener', () => {
+    describe('addListener', () => {
 
         describe('adding when listeners list still empty', () => {
 
-            it('should add have added listener to registry on registerListener call', (done) => {
+            it('should add have added listener to registry on addListener call', (done) => {
 
-                changesNotifier.registerListener(listener1);
+                changesNotifier.addListener(listener1.id, listener1);
 
                 expect(changesNotifier.listenerCount()).toEqual(1);
 
@@ -33,29 +33,29 @@ describe('changesNotifier', () => {
         describe('adding when there are already listeners in the list', () => {
 
             beforeEach((done) => {
-                changesNotifier.registerListener(listener1);
+                changesNotifier.addListener(listener1.id, listener1);
                 done();
             });
 
-            it('should add new listener to registry on registerListener call', (done) => {
-                changesNotifier.registerListener(listener2);
+            it('should add new listener to registry on addListener call', (done) => {
+                changesNotifier.addListener(listener2.id, listener2);
                 expect(changesNotifier.listenerCount()).toEqual(2);
                 done();
             });
         });
     });
 
-    describe('deregisterListener', () => {
+    describe('removeListener', () => {
 
         describe('removing only listener', () => {
 
             beforeEach((done) => {
-                changesNotifier.registerListener(listener1);
+                changesNotifier.addListener(listener1.id, listener1);
                 done();
             });
 
             it('listener should have been removed', (done) => {
-                changesNotifier.deregisterListener(listener1.id);
+                changesNotifier.removeListener(listener1.id);
                 expect(changesNotifier.listenerCount()).toEqual(0);
                 done();
             });
@@ -65,13 +65,13 @@ describe('changesNotifier', () => {
         describe('removing one out of many listeners', () => {
 
             beforeEach((done) => {
-                changesNotifier.registerListener(listener1);
-                changesNotifier.registerListener(listener2);
+                changesNotifier.addListener(listener1.id, listener1);
+                changesNotifier.addListener(listener2.id, listener2);
                 done();
             });
 
             it('listener should have been removed', (done) => {
-                changesNotifier.deregisterListener(listener2.id);
+                changesNotifier.removeListener(listener2.id);
                 expect(changesNotifier.listenerCount()).toEqual(1);
                 done();
             });
