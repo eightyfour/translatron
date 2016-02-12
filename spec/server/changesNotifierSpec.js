c0092b3a72d9f36659fa7093ce96920df7aa4f72
Torben Fruechtenicht <torben.fruechtenicht@gameduell.de>
2016-02-12 14:41:27 +0100

    LODEV-4663 first draft of notify impl

--- spec/server/changesNotifierSpec.js
+++ spec/server/changesNotifierSpec.js
@@ -3,16 +3,23 @@ describe('changesNotifier', () => {
     function newListener() {
         return {
             id :  Math.trunc(Math.random() * Math.pow(10, 10)),
-            onSomethingHappened : function() {}
+            notificationCount : 0,
+            onSomethingHappened : function() {
+                console.log('Client', this.id, 'rcvd notify');
+                this.notificationCount++;
+            }
         };
     };
 
-    var changesNotifier,
-        listener1 = newListener(),
-        listener2 = newListener();
+    var changesNotifier, listener1, listener2;
 
     beforeEach((done) => {
+
+        listener1 = newListener();
+        listener2 = newListener();
+
         changesNotifier = require('../../lib/server/changesNotifier.js')();
+
         done();
     });
 
@@ -73,6 +80,33 @@ describe('changesNotifier', () => {
             it('listener should have been removed', (done) => {
                 changesNotifier.removeListener(listener2.id);
                 expect(changesNotifier.listenerCount()).toEqual(1);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('notify', () => {
+
+        var dummyPayload = {
+            aSimpleValueProperty : 3434.34,
+            aNestedProperty : {
+                aStringProperty : 'asjkdfkl',
+                aIntListProperty : [ 3, 55, 6, 89 ]
+            }
+        };
+
+        describe('notify with only one listener registered', () => {
+
+            beforeEach((done) => {
+                changesNotifier.addListener(listener1.id, listener1);
+                done();
+            });
+
+            it('single registered listener should not receive notification', (done) => {
+                changesNotifier.notify('onSomethingHappened', dummyPayload, listener1.id);
+                expect(listener1.notificationCount).toEqual(0);
                 done();
             });
 
