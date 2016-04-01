45e189d35959df496fac04255da5665e3064f5cc
Fabian Wulf <fabian.wulf@gameduell.de>
2016-04-01 15:19:39 +0200

    updated tests in daoSpec to pass successfully

--- spec/server/daoSpec.js
+++ spec/server/daoSpec.js
@@ -554,11 +554,13 @@ describe('dao', () => {
             });
 
             it('should save description if project had none before', (done) => {
+                var id = '__description';
                 var description = 'testdescription';
-                dao.saveProjectDescription(projectId, description, (err) => {
+                dao.saveProjectDescription(projectId, id, description, (err) => {
                     expect(err).toBeFalsy();
                     dao.loadProject(projectId, (projectData) => {
-                        expect(projectData.description).toEqual(description);
+                        // TODO: Is this intended to be, i.e. key description is empty string?
+                        expect(projectData.keyDescriptions[id]).toEqual(description);
                         done();
                     });
                 });
@@ -583,11 +585,13 @@ describe('dao', () => {
             });
 
             it('should save description if project had one before', (done) => {
+                var id = '__description';
                 var description = 'new_description';
-                dao.saveProjectDescription(projectId, description, (err) => {
+                dao.saveProjectDescription(projectId, id, description, (err) => {
                     expect(err).toBeFalsy();
                     dao.loadProject(projectId, (projectData) => {
-                        expect(projectData.description).toEqual(description);
+                        // TODO: Is this intended to be, i.e. key description is empty string?
+                        expect(projectData.keyDescriptions[id]).toEqual(description);
                         done();
                     });
                 });
