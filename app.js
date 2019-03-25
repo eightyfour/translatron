db3ace70b93d351311d505eeced4767c12438a36
eightyfour <eightyfour@thinkuseful.de>
2019-03-25 18:23:39 +0100

    merge

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var translatron = require('./translatron')
 // start the server with defaults
-<<<<<<< HEAD
 const translatronUi = process.argv[2]
-console.log('translatrionUi', translatronUi)
+
 if (translatronUi) {
     translatron({translatronUi: true});
 } else{
     translatron()
 }
-=======
-translatron()
->>>>>>> 89b08fadb5976e2c67449bf0a1565caff3886827
